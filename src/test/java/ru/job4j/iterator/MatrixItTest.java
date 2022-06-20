2fc70e0c095893e83c70fcadc80d59bcba657bca
khlap <khlaponin.dp@gmail.com>
2022-06-20 11:21:48 +0200

    Итератор для двухмерного массива int[][] [#9539]

--- src/test/java/ru/job4j/iterator/MatrixItTest.java
+++ src/test/java/ru/job4j/iterator/MatrixItTest.java
@@ -8,6 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class MatrixItTest {
+
     @Test
     public void when4El() {
         int[][] in = {
