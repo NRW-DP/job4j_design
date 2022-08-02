cdf91688179e7b2f761a09d996cfae8f610f31f9
khlap <khlaponin.dp@gmail.com>
2022-08-02 22:52:35 +0200

    Реализовать коллекцию Set на массиве [#996 #328245]

--- src/main/java/ru/job4j/set/SimpleSet.java
+++ src/main/java/ru/job4j/set/SimpleSet.java
@@ -4,25 +4,47 @@ import ru.job4j.collection.SimpleArrayList;
 
 import java.util.Iterator;
 
+/**
+ * Класс описывает реализацию коллекции Set на основе динамического массива
+ * Коллекция не содержит дубликаты
+ * @author Ivan H.
+ * @version 1.0
+ */
 public class SimpleSet<T> implements Set<T> {
     private SimpleArrayList<T> set = new SimpleArrayList<>(0);
 
+    /**
+     * Меотод добавляет значение в коллекцию,
+     * если такого значение там нет
+     * @param value - значение
+     * @return true or false
+     */
     @Override
     public boolean add(T value) {
-        boolean check = contains(value);
-        if (!check) {
+        boolean check = !contains(value);
+        if (check) {
             set.add(value);
         }
         return check;
     }
 
+    /**
+     * Метод проверяет наличие элемента в коллекции
+     * @param value - значение
+     * @return - true or false
+     */
     @Override
     public boolean contains(T value) {
+        boolean check = false;
         for (T x : set) {
-            return x.equals(value);
+            if (x == value) {
+                check = true;
+                break;
+            }
         }
-        return false;
+        return check;
     }
+
 
     @Override
     public Iterator<T> iterator() {
