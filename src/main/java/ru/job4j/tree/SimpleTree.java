47ff46dae12a9268f4b8ea9cefc01a5f4de1fbca
khlap <khlaponin.dp@gmail.com>
2022-08-21 19:34:12 +0200

    Создать элементарную структуру дерева [#1711 #334882]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,14 +1,29 @@
 package ru.job4j.tree;
 
 import java.util.*;
-
+/**
+ * Класс описывает элементарную структуру дерева.
+ * По заданию в дереве не могут храниться дубликаты.
+ *
+ * @author Ivan H.
+ * @version 1.0
+ */
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
 
     public SimpleTree(final E root) {
         this.root = new Node<>(root);
     }
-
+    /**
+     * Метод находит узел по значению parent и
+     * добавляет в него дочерний узел со значением child.
+     * Нужно проверить, что parent есть, а child еще нет. Иначе вернуть false.
+     * Проверка идет через метод findBy, который возвращает Optional<Node<E>>.
+     *
+     * @param parent родитель
+     * @param child  потомок
+     * @return true or false
+     */
     @Override
     public boolean add(E parent, E child) {
         boolean rsl;
