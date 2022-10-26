62b360e0880f92d17e311238a035d481c493566f
khlap <khlaponin.dp@gmail.com>
2022-10-26 17:55:47 +0200

    Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,9 +8,25 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
+
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Incorrect number of argument");
+        }
+        validate(args);
+        Path start = Paths.get(args[0]);
+        String extension = args[1];
+        search(start, p -> p.toFile().getName().endsWith(extension)).forEach(System.out::println);
+    }
+
+    public static void validate(String[] validateArg) {
+        Path path = Paths.get(validateArg[0]);
+        if (!Files.exists(path) && Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Invalid directory path.");
+        }
+        if (!validateArg[1].startsWith(".")) {
+            throw new IllegalArgumentException("Wrong format file extension.");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
