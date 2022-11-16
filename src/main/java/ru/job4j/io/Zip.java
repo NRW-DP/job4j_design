b0bf39e32a0a82f7ae2658367e5085a62655a421
khlap <khlaponin.dp@gmail.com>
2022-11-16 15:16:42 +0100

    Архивировать проект [#861 #361346]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,7 +1,9 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.*;
 
@@ -9,9 +11,9 @@ public class Zip {
 
     public void packFiles(List<Path> sources, File target) throws IOException {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (Path source : sources) {
-                zip.putNextEntry(new ZipEntry(source.toFile().getPath()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toFile()))) {
+            for (Path path : sources) {
+                zip.putNextEntry(new ZipEntry(path.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toFile()))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -29,7 +31,30 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
+    private void validate(ArgsName arg) {
+        Path dir = Paths.get(arg.get("d"));
+        if (!Files.isDirectory(dir) || !Files.exists(dir)) {
+            throw new IllegalArgumentException("There's no directory");
+        }
+        if (!arg.get("e").startsWith(".") || arg.get("e").length() == 1) {
+            throw new IllegalArgumentException("Extension of excluding files is not correct");
+        }
+        if (!arg.get("o").endsWith(".zip")) {
+            throw new IllegalArgumentException("Extension of Zip file is not correct");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Amount of arguments is not enough");
+        }
+        ArgsName arg = ArgsName.of(args);
+        Zip zipFiles = new Zip();
+        zipFiles.validate(arg);
+        List<Path> paths = Search.search(Paths.get(arg.get("d")),
+                p -> !p.toFile().getName().endsWith(arg.get("e")));
+        zipFiles.packFiles(paths, new File(arg.get("o")));
+
         Zip zip = new Zip();
         zip.packSingleFile(
                 new File("./pom.xml"),
