7a10dedc7a4963cadf452fda520ad60618bec3df
nrwdp <khlaponin.dp@gmail.com>
2024-02-27 20:37:34 +0100

    2. Что такое Socket? [#4850 #483466]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -2,8 +2,22 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class EchoServer {
+
+    private static Map<String, String> parse(String message) {
+        Map<String, String> params = new HashMap<>();
+        String[] pairs = message.split("&");
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=");
+            if (keyValue.length == 2) {
+                params.put(keyValue[0], keyValue[1]);
+            }
+        }
+        return params;
+    }
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -13,9 +27,12 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String message = input.readLine();
-                    if (message.contains("Bye")) {
+                    String queryString = message.split("\\s")[1].split("\\?")[1];
+                    Map<String, String> params = parse(queryString);
+                    if ("Bye".equals(params.get("msg"))) {
                         server.close();
                     }
+
                     for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
                         System.out.println(string);
                     }
